cfa13b69d1e206f0ef7cb58f11f56111cd8d84f8
Jormaedes Luis <jluis@c3r5s7.42luanda.com>
2026-02-11 11:13:29 +0100

    exibi os dados

--- Day2/variables.py
+++ Day2/variables.py
@@ -10,6 +10,17 @@ age = 190
 year = 2026
 is_married, is_true, is_light_on = False, True, True
 
+print("First name: ", firstname)
+print("Last name: ", lastname)
+print("Full name: ", fullname)
+print("Country: ", country)
+print("City: ", city)
+print("Age: ", age)
+print("Year: ", year)
+print("Is married: ", is_married)
+print("Is True: ", is_true)
+print("Is light on: ", is_light_on)
+
 ## Exercicios: Level 2
 print(type(firstname))
 print(type(lastname))
@@ -40,10 +51,16 @@ _circum_of_circle_ = 2 * 3.14 * r
 
 r = float(input("Enter the radius: "))
 _area_of_circle_ = 3.14 * r * r
+print("Area of Circle: ", _area_of_circle_)
 
 firstname = input("Enter your firstname: ")
 lastname = input("Enter your lastname: ")
 country = input("Enter your country: ")
 age = int(input("Enter your age: "))
+print("First name: ", firstname)
+print("Last name: ", lastname)
+print("Country: ", country)
+print("Age: ", age)
+
 
 help('keywords')
