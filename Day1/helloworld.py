19c48c1d8052f82ed7568cbdde1486a1bb66c297
Jormaedes Luis <jluis@c3r5s7.42luanda.com>
2026-02-11 10:23:19 +0100

    resolvi os exercios do dia 1

---
+++ Day1/helloworld.py
@@ -0,0 +1,39 @@
+# Day 1 - 30DaysOfPython Challenge
+
+# Exercicio: Level 2
+#-------------
+
+## Perguntas 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 % 4)
+print(3 // 4)
+
+## Perguntas 3
+print("Jormaedes") # Nome
+print("Luís") # Sobrenome
+print("Pai: Jorge Luís")
+print("Mãe: Ana Samuel")
+print("Eu estou aproveitando o 30 dias de python") # Eu estou aproveitando o 30 dias de python
+
+## Perguntas 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Jormaedes")) # nome
+print(type("Luís")) ## Sobrenome
+print(type("Pai: Jorge Luís")) # Pai
+print(type("Mãe: Ana Samuel")) # Mãe
+
+
+# Exercicio: Level 3
+# Pergunta 1
+print(['c', 'cpp', 'java', 'javascript', 'python'])
+
+# Pergunta 2
+print((((10 - 2) ** 2) + ((8 - 3) ** 2)) ** 0.5)
