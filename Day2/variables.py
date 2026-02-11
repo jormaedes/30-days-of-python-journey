55e7d6b6fec45c3d55484e6b9b0c27c517a38f89
Jormaedes Luis <jluis@c3r5s7.42luanda.com>
2026-02-11 11:05:26 +0100

    Exercicio feito com sucesso

---
+++ Day2/variables.py
@@ -0,0 +1,49 @@
+# Dia 2/30 dias de programação em python
+
+## Exercicios: Level 1
+firstname = "Jormaedes"
+lastname = "Luís"
+fullname = firstname + " " + lastname
+country = "Angola"
+city = "Luanda"
+age = 190
+year = 2026
+is_married, is_true, is_light_on = False, True, True
+
+## Exercicios: Level 2
+print(type(firstname))
+print(type(lastname))
+print(type(fullname))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print('len: ', len(firstname))
+print(len(firstname) > len(lastname))
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+prod = num_two * num_one
+div = num_one / num_two
+rest = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+r = 30
+_area_of_circle_ = 3.14 * r * r
+_circum_of_circle_ = 2 * 3.14 * r
+
+r = float(input("Enter the radius: "))
+_area_of_circle_ = 3.14 * r * r
+
+firstname = input("Enter your firstname: ")
+lastname = input("Enter your lastname: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+
+help('keywords')
